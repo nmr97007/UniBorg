3dc0791b4f4f07aa874097a26c9724640bb41c38
nmr97007 <55343518+nmr97007@users.noreply.github.com>
2019-11-16 19:10:04 +0530

    Update pic.py

--- stdplugins/pic.py
+++ stdplugins/pic.py
@@ -77,12 +77,12 @@ async def autopic(event):
         drawn_text.text((x,y), cn_time, font=fnt, fill=(0, 102, 204))
         #bottom
         #outline
-        drawn_text.text((x2+3,y), ct_time, font=fnt, fill=black)
-        drawn_text.text((x2-3,y), ct_time, font=fnt, fill=black)
-        drawn_text.text((x,y2+3), ct_time, font=fnt, fill=black)
-        drawn_text.text((x,y2-3), ct_time, font=fnt, fill=black)
+        drawn_text.text((x2+1,y), ct_time, font=fnt2, fill=black)
+        drawn_text.text((x2-1,y), ct_time, font=fnt2, fill=black)
+        drawn_text.text((x,y2+1), ct_time, font=fnt2, fill=black)
+        drawn_text.text((x,y2-1), ct_time, font=fnt2, fill=black)
         #main        
-        drawn_text.text((x2,y2), ct_time, font=fnt, fill=(255, 0, 0))
+        drawn_text.text((x2,y2), ct_time, font=fnt2, fill=(255, 0, 0))
         #draw_complete
         img.save(photo)
 
