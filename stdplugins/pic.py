14d41c3b552f2dffbef13ce75ab500cae2eb0ef8
nmr97007 <55343518+nmr97007@users.noreply.github.com>
2019-11-17 17:52:11 +0530

    Update pic.py

--- stdplugins/pic.py
+++ stdplugins/pic.py
@@ -1,57 +1,31 @@
 import os
-
 from datetime import datetime
-
 from pytz import timezone
-
 from PIL import Image, ImageDraw, ImageFont, ImageFile
-
 from pySmartDL import SmartDL
-
 from telethon.tl import functions
-
 from uniborg.util import admin_cmd
-
 import asyncio
-
 import shutil
 
 FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
 VERY_PIC = "http://lorempixel.com/500/500/" 
 
 @borg.on(admin_cmd(pattern="autopp"))
-
 async def autopic(event):
-
     downloaded_file_name = "./DOWNLOADS/original_pic.png"
-
-    downloader = SmartDL(VERY_PIC, downloaded_file_name, progress_bar=False)
-
-    downloader.start(blocking=False)
-
-    photo = "photo_pfp.png"
-
-    while not downloader.isFinished():
-
-        place_holder = None
-
-    counter = -5
-
+    photo = "photo.png" 
     while True:
         downloaded_file_name = "./DOWNLOADS/original_pic.png"
-
         downloader = SmartDL(VERY_PIC, downloaded_file_name, progress_bar=False)
-
         downloader.start(blocking=False)
-        await asyncio.sleep(30)
+        while not downloader.isFinished():
+           place_holder = None
+        counter = -30
         shutil.copy(downloaded_file_name, photo)
-        
-        ImageFile.LOAD_TRUNCATED_IMAGES = True
         im = Image.open(photo)
         file_test = im.save(photo, "PNG")
-
         now_utc = datetime.now(timezone('UTC'))
-
         now_asia = now_utc.astimezone(timezone('Asia/Kolkata'))
         x = 350
         y = 15
@@ -62,9 +36,7 @@ async def autopic(event):
         cn_time = now_asia.strftime("%d %b %Y\n@user_nmr")
         ct_time = now_asia.strftime("%I:%M %p")
         img = Image.open(photo)
-
         drawn_text = ImageDraw.Draw(img)
-
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 20)
         fnt2 = ImageFont.truetype(FONT_FILE_TO_USE, 40)
         #top_part
@@ -85,23 +57,13 @@ async def autopic(event):
         drawn_text.text((x2,y2), ct_time, font=fnt2, fill=(255, 0, 0))
         #draw_complete
         img.save(photo)
-
         file = await event.client.upload_file(photo)  # pylint:disable=E0602
-
         try:
-
             await event.client(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
-
                 file
-
             ))
-
             os.remove(downloaded_file_name)
-
-            counter -= 5
-
-            await asyncio.sleep(35)
-
+            counter -= 30
+            await asyncio.sleep(65)
         except:
-
             return
